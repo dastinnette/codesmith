9724d35446627d2c3fc2e47d30adf0fe2d6a7b5b
David Stinnette <dastinnette@gmail.com>
2023-07-02 15:50:32 -0400

    Completed fizzbuzz application challenge

---
+++ fizzbuzz.js
@@ -0,0 +1,18 @@
+const results = [];
+
+function fizzbuzz(n) {
+  for (var i=1; i<=n; i++) {
+    if (i%3 == 0 && i%5 == 0) {
+  		results.push("fizzbuzz")
+  	} else if (i%3 == 0) {
+    	results.push("fizz")
+  	} else if (i%5 == 0) {
+    	results.push("buzz")
+  	} else {
+    results.push(i)
+  	}
+  }
+};
+
+fizzbuzz(16);
+console.log(results);
